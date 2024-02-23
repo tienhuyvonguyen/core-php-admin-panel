729ab49d1a959889ebfe355af9026331ade062b9
Huy Vo <tienhuyvonguyen@gmail.com>
2024-02-23 13:59:57 +0700

    modified:   config/config.php

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ define('APP_FOLDER', 'simpleadmin');
 // define time zone for the whole application
 date_default_timezone_set('Asia/Bangkok');
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
-define('BASE_URL', 'https://thevolunty.azurewebsites.net');
+define('BASE_URL', 'https://exe20240123205125.azurewebsites.net/');
 require_once BASE_PATH . '/lib/MysqliDb/MysqliDb.php';
 require_once BASE_PATH . '/helpers/helpers.php';
 
