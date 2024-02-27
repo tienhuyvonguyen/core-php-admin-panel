5ef8fe09bc3774b1570a70724c14542941bc9b88
Huy Vo <tienhuyvonguyen@gmail.com>
2024-02-27 16:36:05 +0700

    modified:   config/config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,4 @@
 <?php
-
 //Note: This file should be included first in every php page.
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
@@ -8,7 +7,5 @@ define('APP_FOLDER', 'simpleadmin');
 // define time zone for the whole application
 date_default_timezone_set('Asia/Bangkok');
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
-define('BASE_URL', 'https://exe20240123205125.azurewebsites.net/');
-require_once BASE_PATH . '/lib/MysqliDb/MysqliDb.php';
-require_once BASE_PATH . '/helpers/helpers.php';
+define('BASE_URL', 'https://backendthevolunty.azurewebsites.net/');
 
