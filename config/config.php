239f66d352e65d4754f896cde16b3d30b7d95655
Huy Vo <tienhuyvonguyen@gmail.com>
2024-03-01 08:44:10 +0700

    modified:   config/config.php

--- config/config.php
+++ config/config.php
@@ -2,13 +2,11 @@
 
 //Note: This file should be included first in every php page.
 error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+ini_set('display_errors', 'Off');
 define('BASE_PATH', dirname(dirname(__FILE__)));
 define('APP_FOLDER', 'simpleadmin');
 // define time zone for the whole application
 date_default_timezone_set('Asia/Bangkok');
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
 define('BASE_URL', 'https://exe20240123205125.azurewebsites.net/');
-require_once BASE_PATH . '/lib/MysqliDb/MysqliDb.php';
-require_once BASE_PATH . '/helpers/helpers.php';
 
